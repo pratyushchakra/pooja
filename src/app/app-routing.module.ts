b4874a7fa269dd3ad2bd6d4e361c158209ef1ab1
Pratyush <pratyushkumarchakra@gmail.com>
2018-12-08 02:14:24 +0530

    routes and templates set

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component'
+import {ContactUsComponent} from './components/contact-us/contact-us.component'
+import {FeaturesComponent} from './components/features/features.component' 
+const routes: Routes = [
+  {
+    path : 'home',
+    component: HomepageComponent
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'contact_us',
+    component: ContactUsComponent
+  },
+  {
+    path: 'features',
+    component: FeaturesComponent
+  }
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { FeaturesComponent } from './components/features/features.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    NavbarComponent,
+    ContactUsComponent,
+    FeaturesComponent
   ],
   imports: [
     BrowserModule,
